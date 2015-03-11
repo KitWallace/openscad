6ad9e4d8b178282c3a7849334de11264b5ef96fe
Chris Wallace <kit.wallace@gmail.com>
2015-03-11 08:56:47 +0000

    Create image-to-openscad.py

---
+++ image-to-openscad.py
@@ -0,0 +1,45 @@
+""" convert from a JPEG image to an openSCAD array declaration
+    for inclusion in an openSCAD script
+  parameters
+    1 image file name
+    2  width in units of the scaled image
+    3  0 = normal, 1 = invented
+    output openscad code on stdout
+  
+   uses  PIL, numpy
+   Chris Wallace March 2015
+  
+"""
+
+
+from PIL import Image
+import numpy,sys
+
+def surface_to_openSCAD (surface,name,f) :
+    f.write( 'image_file = "'  + name   + '";\n')
+    maxi = surface.shape[1]-1
+    maxj = surface.shape[0]-1
+    f.write('image = [ \n")
+    for i in range(0, maxi) :
+       f.write("[ \n")
+       for j in range(0, maxj) :
+          f.write( str(surface[j][i]) + ", ")
+       f.write("],\n")
+    f.write("];\n")
+
+file = sys.argv[1]
+width = float(sys.argv[2])
+invert = int(sys.argv[3])
+
+pic = Image.open(file)
+dim = pic.size
+ratio =  width /dim[0]
+dim_resize = (int(dim[0]*ratio),int(dim[1]*ratio)) 
+pic_resize = pic.resize(dim_resize,Image.ANTIALIAS)
+pic_greyscale = pic_resize.convert("L")
+pic_array = numpy.array(pic_greyscale).reshape((dim_resize[1],dim_resize[0]))
+pic_array2 = pic_array / 256.0
+if invert == 1 :
+   pic_array2 = 1 - pic_array2
+   
+surface_to_openSCAD(pic_array2,file,sys.stdout)
